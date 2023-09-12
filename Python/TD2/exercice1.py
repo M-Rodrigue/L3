8e59ef3887e4381590ca31019e7c41e6b4d603a8
M-Rodrigue <morel.rodrigue@protonmail.com>
2023-09-12 11:50:45 +0200

    Python - TD2

---
+++ Python/TD2/exercice1.py
@@ -0,0 +1,2 @@
+# Exercice 1 - Client-serveur jeu trouve
+# Ecrire un jeu qui permet à un client de deviner un nombre aléatoire choisi par le serveur. Le client indique le nombre maximum au serveur. Le serveur demande un nombre au client et répond ‘plus’ ou ‘moins’. La connexion est terminée lorsque le nombre est trouvé.
