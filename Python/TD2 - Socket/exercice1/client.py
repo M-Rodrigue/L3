4e8b83f2bca2853308ff221ca71fc46e1b7ef3cb
M-Rodrigue <morel.rodrigue@protonmail.com>
2023-09-13 16:29:52 +0200

    Python - TD2

--- Python/TD2 - Socket/exercice1/client.py
+++ Python/TD2 - Socket/exercice1/client.py
@@ -12,13 +12,22 @@ def connexion():
   return s
 
 def client(s):
-  while True:
-    nombre = input("Donnez un nombre (ou '0' pour quitter) : ")
-    if nombre == 0:
-      break
-    s.send(nombre.encode()) #2
-    data = s.recv(128).decode() #3
-    print(data)
+  nombreMax = int(input("Veuillez donner un nombre limite pour commencer (quitter le programme : 0) : "))
+  s.send(str(nombreMax).encode()) #2
+
+  if(nombreMax != 0):
+    while True:
+      data = s.recv(128) #3
+      print(data.decode())
+
+      nombre = int(input("Donner un nombre : "))
+      s.send(str(nombre).encode()) #4
+
+      data = s.recv(128) #5
+      print(data.decode())
+
+      if nombre == 0:
+        break
 
 def main():
   s = connexion()
@@ -26,3 +35,30 @@ def main():
   s.close()
 
 main()
+
+# ############################# CORRECTION ###############################
+# import socket, time
+
+# host="127.0.0.1"
+# port = 1338
+# s=socket.socket()
+# s.connect((host,port))
+# data=s.recv(1024)
+# print(data.decode())
+# nbr0=input()
+# s.send(nbr0.encode("utf8"))
+# reponse="test"
+# point=0
+# while ( True ):
+#     if (reponse!="fini"):
+#         nbr=int(input("entrez un nombre\n"))
+#         s.send(str(nbr).encode("utf8"))
+#         reponse=s.recv(10)
+#         reponse=reponse.decode("utf8")
+#         print(reponse)
+#         point=point+1
+#     else:
+#         print("c'est fini")
+#         print("vous avez gagné en ",point," coups")
+#         break
+# s.close()
--- Python/TD2 - Socket/exercice1/serveur.py
+++ Python/TD2 - Socket/exercice1/serveur.py
@@ -11,32 +11,71 @@ def serveur():
   s.listen()
   s_client, adresse = s.accept()
   print("Client " + str(adresse) + " s'est connecté")
-  s_client.send(("Client " + str(adresse) + " s'est connecté\r\n").encode()) #1
+  s_client.send(("Client " + str(adresse) + " s'est connecté").encode()) #1
   return s, s_client
 
 def juste_prix(s, s_client):
-  s_client.send(b"Veuillez donner un nombre limite\r\n") #2
   nombreMax = s_client.recv(128).decode() #2
-  nombreDeBase = random.randint(1, int(nombreMax))
-  s_client.send(("Vous devez deviner le nombre aléatoire entre 1 et " + str(nombreMax) + "\r\n").encode())
 
-  i = 0
-  nombreUtilisateur = 0
-  while int(nombreUtilisateur) != nombreDeBase:
-    i += 1
-    s_client.send(b"Donner un nombre : \r\n")
-    nombreUtilisateur = int(input(s_client.recv(128).decode()))
-    if(int(nombreUtilisateur) < nombreDeBase):
-      s_client.send(b"Plus !\r\n")
-    elif(int(nombreUtilisateur) > nombreDeBase):
-      s_client.send(b"Moins !\r\n")
 
-  s_client.send(("Vous avez trouvé le nombre en " + str(i) + " tentatives\r\n").encode())
+  if(nombreMax != 0):
+    nombreDeBase = random.randint(1, int(nombreMax))
+    print("Le nombre à trouver est : " + str(nombreDeBase))
+
+    s_client.send(("Vous devez deviner le nombre aléatoire entre 1 et " + str(nombreMax)).encode()) #3
+
+    i = 0
+    nombreUtilisateur = 0
+    while int(nombreUtilisateur) != nombreDeBase:
+      i += 1
+
+      nombreUtilisateur = s_client.recv(128).decode() #4
+
+      if(int(nombreUtilisateur) < nombreDeBase):
+        s_client.send("Plus !".encode()) #5
+      elif(int(nombreUtilisateur) > nombreDeBase):
+        s_client.send("Moins !".encode()) #5
+
+    s_client.send(("Vous avez trouvé le nombre en " + str(i) + " tentatives").encode()) #5
+
+def main():
+  print("Serveur en cours")
+  s, s_client = serveur()
+  juste_prix(s, s_client)
   s_client.close()
   s.close()
 
-def main():
-  s, s_client = serveur()
-  juste_prix(s, s_client)
-
 main()
+
+
+# ############################# CORRECTION ###############################
+# import socket, time, random
+
+# host=""
+# port=1337
+# s=socket.socket()
+# s.bind((host,port))
+# s.listen(1)
+# client,address=s.accept()
+# client.send("\n\n===================\n\nbienvenue dans ce jeu\nVeuillez donner un nombre maximum pour le tirage aleatoire\nBonne chance\n===================\n\n".encode("utf8"))
+# n=client.recv(8)
+# nbr=int(n)
+# print ("le nombre maximum est : ",nbr)
+# alea=random.randint(1,nbr)
+# print ("le tirage donne : ",alea)
+# nbr_c=client.recv(10)
+# nbr_c=int(nbr_c)
+# print("le nombre recu est : ", nbr_c)
+# while ( int(nbr_c) != alea ):
+#     if ( int(nbr_c) < alea):
+#         client.send("plus".encode("utf8"))
+#     else:
+#         client.send("moins".encode("utf8"))
+#     nbr_c = client.recv(10)
+#     nbr_c=nbr_c.decode("utf8")
+#     print("le nombre recu est : ",nbr_c)
+# client.send("fini".encode("utf8"))
+# time.sleep(1)
+# client.send("vous avez trouve\n".encode("utf8"))
+# client.close()
+# s.close()
