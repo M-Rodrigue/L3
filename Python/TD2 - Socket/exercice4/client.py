528abdcb7e7393df6bacb662c5a80a89d679f232
M-Rodrigue <morel.rodrigue@protonmail.com>
2023-09-13 17:18:42 +0200

    Python - TD2 - Correction

---
+++ Python/TD2 - Socket/exercice4/client.py
@@ -0,0 +1,54 @@
+
+############################# CORRECTION ###############################
+import socket
+import time
+def generate_words():
+    alphabet = [chr(i) for i in range(ord('a'), ord('z')+1)]
+    words = []
+
+    for word_length in range(1, 5):
+        for letter1 in alphabet:
+            words.append(letter1)
+            for letter2 in alphabet:
+                words.append(letter1 + letter2)
+                for letter3 in alphabet:
+                    words.append(letter1 + letter2 + letter3)
+                    for letter4 in alphabet:
+                        words.append(letter1 + letter2 + letter3 + letter4)
+
+    return words
+
+def client_program():
+	host = socket.gethostname()
+	port = 3837
+
+	client_socket = socket.socket()
+	client_socket.connect((host, port))
+
+	message = client_socket.recv(1024).decode()
+	print(f"Serveur : {message}")
+
+	client_socket.send("admin".encode())
+	message = client_socket.recv(1024).decode()
+	print(f"Serveur : {message}")
+
+	alphabet = [chr(i) for i in range(ord('a'), ord('z')+1)]
+	start_time = time.time()
+
+	liste=[]
+	liste=generate_words()
+	for password in liste:
+		print("password :",password)
+		client_socket.send(password.encode())
+		message = client_socket.recv(1024).decode()
+		print(f"Serveur : {message}")
+		if message == "Connexion réussie !":
+			end_time = time.time()
+			print(f"Mot de passe trouvé : {password}")
+			print(f"Temps écoulé : {end_time - start_time} secondes")
+			return
+
+	client_socket.close()
+
+if __name__ == "__main__":
+    client_program()
