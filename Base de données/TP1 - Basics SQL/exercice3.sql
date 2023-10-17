fec581049626b1cb2ab409b5c347e9c0726b9268
M-Rodrigue <morel.rodrigue@protonmail.com>
2023-10-17 23:52:34 +0200

    Base de données - TP1

---
+++ Base de données/TP1 - Basics SQL/exercice3.sql
@@ -0,0 +1,7 @@
+insert into employe (id_emp, nom, prenom, sexe, service, salaire, dtContrat) values
+	(1, 'DUPONT', 'Robert', 'M', 'Marketing', 20000, '21-03-2010'),
+	(2, 'DUPONT', 'Aline', 'F', 'Communication', 8500, '11-11-2013'),
+	(3, 'DURAND', 'Laurence', 'F', 'Marketing', 14000, '02-01-1996'),
+	(4, 'LEJEUNE', 'Sylvie', 'F', 'Marketing', 21500, '02-09-2018'),
+	(5, 'LEFORT', 'Max', 'M', NULL, 12000, '11-09-2005')
+;
